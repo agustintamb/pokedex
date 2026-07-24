f2e46c9f855d62b92d88d3247e260995a4537158
Agustin <agustingonzaleztam@gmail.com>
2026-07-24 11:42:17 -0300

    Installed vitest + rtl

--- vite.config.js
+++ vite.config.js
@@ -10,4 +10,15 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  test: {
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.js',
+    css: true,
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+      include: ['src/**/*.{js,jsx}'],
+      exclude: ['src/main.jsx', 'src/**/*.test.{js,jsx}'],
+    },
+  },
 })
